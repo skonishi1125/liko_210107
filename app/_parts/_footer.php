d7b2c571c2e14ba15359513141977cc5df8911b3
skonishi1125 <skonishi1125@gmail.com>
2021-01-21 15:13:02 +0900

    ・Ajax実装のため、jqueryをslim版から通常版に変更 ・ajax用jsファイルの生成 ・footerパーツにajax用jsファイルの読み込みを設定

--- app/_parts/_footer.php
+++ app/_parts/_footer.php
@@ -1,7 +1,8 @@
 <!-- メインのフッター -->
-<script src="../js/jquery-3.5.1.slim.min.js"></script>
+<script src="../js/jquery-3.5.1.min.js"></script>
 <script src="../js/bootstrap.bundle.min.js"></script>
 <script src="../js/script.js"></script>
+<script src="../js/ajaxGoods.js"></script>
 
 </body>
 </html>
