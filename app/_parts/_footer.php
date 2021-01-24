b9f4a74156fe2ba6034ebe0a16193a0f4b5c581a
skonishi1125 <skonishi1125@gmail.com>
2021-01-24 16:50:04 +0900

    更新されたJS、CSSのブラウザキャッシュを破棄して再度読み込ませるように変更。

--- app/_parts/_footer.php
+++ app/_parts/_footer.php
@@ -1,8 +1,8 @@
 <!-- メインのフッター -->
 <script src="../js/jquery-3.5.1.min.js"></script>
 <script src="../js/bootstrap.bundle.min.js"></script>
-<script src="../js/script.js"></script>
-<script src="../js/ajaxGoods.js"></script>
+<script src="../js/script.js?<?= filemtime('../js/script.js'); ?>"></script>
+<script src="../js/ajaxGoods.js?<?= filemtime('../js/ajaxGoods.js'); ?>"></script>
 
 </body>
 </html>
--- join/app/_parts/_header.php
+++ join/app/_parts/_header.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <!-- bootstrap v4.5 -->
   <link rel="stylesheet" href="../css/bootstrap.min.css">
-  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/style.css?<?= filemtime('../css/style.css'); ?>">
   <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
   <title>Liko</title>
 </head>
